e7e0037e984f5699e5d96366032246d7b7d8b67c
demolab <bellofaisol@gmail.com>
2023-03-01 15:59:15 +0100

    Second Screening Modification (student_affair)

--- student_affair_officers/urls.py
+++ student_affair_officers/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.homepage, name='home'),
     path('first-screening', views.FirstScreeningList.as_view(), name='first_screening'),
     path('first-screening/<int:pk>/', views.FirstScreeningDetail.as_view(), name='first_screening_detail'),
-    path('first-screening-modify/<int:id>/', views.fscreening_modify, name='modify_first_screening')
-    
+    path('first-screening-modify/<int:id>/', views.fscreening_modify, name='modify_first_screening'),
+    path('second-screening/', views.SecondScreeningList.as_view(), name='second_screening'),
+    path('second-screening/<int:pk>', views.SecondScreeningDetail.as_view(), name='second_screening_detail'),
+    path('second-screening-modify/<int:id>/', views.sscreening_modify, name='second_screening_modification'),
 ]
--- student_affair_officers/views.py
+++ student_affair_officers/views.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from screening.models import FirstScreening
+from screening.models import FirstScreening, SecondScreening
 
 
 # Create your views here.
 def homepage(request):
     return render(request, 'student_affair_officers/homepage.html', context={})
 
-class FirstScreeningList(ListView):
+class FirstScreeningList(LoginRequiredMixin, ListView):
     model = FirstScreening
     template_name = 'student_affair_officers/first_screening.html'
     context_object_name = "fscreening"
@@ -24,6 +25,7 @@ class FirstScreeningDetail(DetailView):
     model = FirstScreening
     template_name = 'student_affair_officers/first_screening_detail.html'
 
+@login_required
 def fscreening_modify(request, id):
     if request.method == 'POST':
         if request.POST.get('approve'):
@@ -45,4 +47,40 @@ def fscreening_modify(request, id):
                 fscreening.save()
                 messages.success(request, 'Screening successfully rejected')
     return redirect(reverse('student_affair:first_screening'))
+    
+class SecondScreeningList(LoginRequiredMixin, ListView):
+    model = SecondScreening
+    template_name = 'student_affair_officers/second_screening.html'
+    context_object_name = 'sscreening'
+
+    def get_queryset(self):
+        return SecondScreening.objects.filter(status = 'pending for student affair')
+
+class SecondScreeningDetail(LoginRequiredMixin, DetailView):
+    model = SecondScreening
+    template_name = 'student_affair_officers/second_screening_detail.html'
+
+@login_required
+def sscreening_modify(request, id):
+    if request.method == 'POST':
+        #print(request.POST)
+        if request.POST.get('approve'):
+            screening = SecondScreening.objects.get(id=id)
+            screening.status = 'approved'
+            screening.comment = 'Approved'
+            screening.save()
+            messages.success(request, 'Second Screening has been approved')
+        elif request.POST.get('reject'):
+            if request.POST.get('comment') == '':
+                messages.warning(request, "Kindly state reasons why you are rejecting this request")
+                print('Kindly state reasons why you are rejecting this request')
+                return redirect(request.path_info) #not working as i want, should be redirecting to the same page (reload)
+            screening = SecondScreening.objects.get(id=id)
+            screening.status = 'rejected'
+            screening.comment = request.POST.get('comment')
+            # print(request.POST.get('comment'))
+            screening.save()
+            messages.success(request, 'Screening rejected')
+
+    return redirect(reverse('student_affair:second_screening'))
     
