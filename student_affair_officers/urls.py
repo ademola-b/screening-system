906b0a74a7fb5bd214431e6d9950787089298fd4
demolab <bellofaisol@gmail.com>
2023-03-14 10:53:54 +0100

    Change in urls

--- student_affair_officers/urls.py
+++ student_affair_officers/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('first-screening/<int:pk>/', views.FirstScreeningDetail.as_view(), name='first_screening_detail'),
     path('first-screening-modify/<int:id>/', views.fscreening_modify, name='modify_first_screening'),
     path('second-screening/', views.SecondScreeningList.as_view(), name='second_screening'),
-    path('second-screening/<int:pk>', views.SecondScreeningDetail.as_view(), name='second_screening_detail'),
+    path('second-screening/<int:pk>/', views.SecondScreeningDetail.as_view(), name='second_screening_detail'),
     path('second-screening-modify/<int:id>/', views.sscreening_modify, name='second_screening_modification'),
 ]
